59590f4c18384a628713f5f443c12ee2e0d042c6
Fedor Sulitskiy <72353528+fedorSulitskiy@users.noreply.github.com>
2022-11-21 10:59:47 +0000

    sum exrcises

---
+++ Databases_0178/Exercise Files/Chap01/db-connect.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# Copyright 2021 BHG [bw.org]
+# as of 2021-04-07 bw
+
+import mysql.connector as mysql
+
+
+def main():
+    db = mysql.connect(host = "localhost",user="root", password = "Constantinople1452")
+    cur = db.cursor()
+
+    cur.execute("SELECT VERSION()")
+    version = cur.fetchone()[0]
+    print(f"MySQL version {version}")
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ Databases_0178/Exercise Files/Chap01/db-query.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# Copyright 2021 BHG [bw.org]
+# db-query.py as of 2021-04-07 bw
+
+import sqlite3
+
+
+def main():
+    db = sqlite3.connect(r"C:\Users\fsuli\Programming\02UNI\Databases_0178\Exercise Files\db\scratch.db")
+    cur = db.cursor()
+
+    cur.execute("DROP TABLE IF EXISTS temp")
+    cur.execute("CREATE TABLE IF NOT EXISTS temp ( a TEXT, b TEXT, c TEXT )")
+    cur.execute("INSERT INTO temp VALUES ('one', 'two', 'three')")
+    cur.execute("INSERT INTO temp VALUES ('four', 'five', 'six')")
+    cur.execute("INSERT INTO temp VALUES ('seven', 'eight', 'nine')")
+    db.commit()
+
+    cur.execute("SELECT * FROM temp")
+    for row in cur:
+        print(row)
+
+    cur.execute("DROP TABLE IF EXISTS temp")
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
